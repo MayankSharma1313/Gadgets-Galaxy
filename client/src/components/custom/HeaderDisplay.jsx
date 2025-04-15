a813e5c9f9a93148f261ad6d2f37d8c77b7fec28
Mayank <ms1829399@gmail.com>
2025-04-16 04:55:31 +0530

    add images

--- client/src/components/custom/HeaderDisplay.jsx
+++ client/src/components/custom/HeaderDisplay.jsx
@@ -1,42 +1,47 @@
-import React from 'react'
+import React from "react";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel"
-import Autoplay from "embla-carousel-autoplay"
-
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 export const HeaderDisplay = () => {
+  const imagesData = [
+    "https://m.media-amazon.com/images/I/81tioCUVf4L._AC_SX296_SY426_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/41HFqEr055L._AC_SR100,100_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/61HBpQuLjVL._AC_SX296_SY426_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/61QNaY7PMpL._AC_SR526,526_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/61vrPzaPnSL._AC_SR526,526_FMwebp_QL65_.jpg",
+  ];
 
-    const imagesData = [
-        "images/2.jpg","images/1.jpg","images/3.jpg","images/4.jpg",
-    ]
-
-    return (
-        <div>
-            <Carousel className='my-10 mx-auto w-[93vw] overflow-x-clip sm:overflow-x-visible'
-             plugins={[
-        Autoplay({
-          delay: 2000,
-        }),
-      ]}>
-                <CarouselContent>
-                   {
-                    imagesData.map((image, index) => (
-                        <CarouselItem key={index}>
-                            <img src={image} loading='lazy' className='object-cover w-full h-[60vh] rounded-3xl' alt="img" />
-                        </CarouselItem>
-                    ))
-                   }
-                    
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
-
-        </div>
-    )
-}
+  return (
+    <div>
+      <Carousel
+        className="my-10 mx-auto w-[93vw] overflow-x-clip sm:overflow-x-visible"
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {imagesData.map((image, index) => (
+            <CarouselItem key={index}>
+              <img
+                src={image}
+                loading="lazy"
+                className="object-cover w-full h-[60vh] rounded-3xl"
+                alt="img"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
